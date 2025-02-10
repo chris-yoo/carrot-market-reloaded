2dd3e8f276abdbf16bf7140a1ccfefca318817f3
chris-yoo <ysm020423@gmail.com>
2025-02-10 22:54:46 +0900

    zod error messege

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,7 +1,12 @@
 "use server";
 import { z } from "zod";
 
-const usernameSchema = z.string().min(5).max(10);
+const formSchema = z.object({
+  username: z.string().min(3).max(10),
+  email: z.string().email(),
+  password: z.string().min(10),
+  confirm_password: z.string().min(10),
+});
 
 export async function createAcount(prevState: any, formData: FormData) {
   const data = {
@@ -10,7 +15,18 @@ export async function createAcount(prevState: any, formData: FormData) {
     password: formData.get("password"),
     confirm_password: formData.get("confirm_password"),
   };
-  console.log(data);
+  //   console.log(data);
+  //   try {
+  //     formSchema.parse(data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  //parse 는 에러를 던지지만 safeParse 는 에러를 반환하지 않는다.
+  const result = formSchema.safeParse(data);
 
-  usernameSchema.parse(data.username);
+  console.log(result);
+  if (!result.success) {
+    // console.log(result.error.errors); 이렇게 써야 에러가 안난다. result.error 그 자체로는 에러가 난다.
+    return result.error.flatten();
+  }
 }

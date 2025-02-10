7b7c9a78310a0198de41197e093b3a56db557b36
chris-yoo <ysm020423@gmail.com>
2025-02-10 23:20:27 +0900

    zod custom error messege

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,12 +1,29 @@
 "use server";
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
+const checkPasswords = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
+
+const formSchema = z
+  .object({
+    username: z
+      .string()
+      .min(3, "Way too short!!!")
+      .max(10, "That is to long!!!!!")
+      .refine((username) => false, "custom error"),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirm_password: z.string().min(10),
+  })
+  .refine(checkPasswords, {
+    message: "Both passwords should be the same!",
+    path: ["confirm_password"],
+  });
 
 export async function createAcount(prevState: any, formData: FormData) {
   const data = {
@@ -27,6 +44,7 @@ export async function createAcount(prevState: any, formData: FormData) {
   console.log(result);
   if (!result.success) {
     // console.log(result.error.errors); 이렇게 써야 에러가 안난다. result.error 그 자체로는 에러가 난다.
+    console.log(result.error.flatten());
     return result.error.flatten();
   }
 }

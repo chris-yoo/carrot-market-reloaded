560bc001ea8a8f68f9c5aaa57a83873e216ed618
chris-yoo <ysm020423@gmail.com>
2025-02-10 22:09:47 +0900

    zod intro

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,8 +4,8 @@ import FormInput from "../components/form-input";
 import FormButton from "../components/form-btn";
 import SocialLogin from "../components/social-login";
 import { redirect } from "next/navigation";
-import { useFormState } from "react-dom";
 import { handleForm } from "./actions";
+import { useActionState } from "react";
 
 export default function Login() {
   //   const onClick = async () => {
@@ -19,12 +19,12 @@ export default function Login() {
   //     console.log(await response.json());
   //   };
 
-  const [state, action] = useFormState(handleForm, { potato: 1 } as any);
+  const [state, action] = useActionState(handleForm, { potato: 1 } as any);
 
   //const { action, data, method, pending } = useFormStatus();
   //규칙 action property 가 할당된 하위 component 에만 사용가능
   // initail state 는 action 이 반환하는 것과 형태가 같아야 한다.
-
+  // useFormState -> useActionState 로 변경
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
